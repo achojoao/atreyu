201bbc15d5d903afc258a24378bc76dcd2a75162
achojoao <juanvidom@gmail.com>
2018-05-15 20:52:01 +0200

    Moving keys and functionality

--- atreyu.sh
+++ atreyu.sh
@@ -20,34 +20,31 @@ EOD)
     if [ "$APP" != "$CURRENT_APP" ]
     then
         CURRENT_APP="$APP"
-        TRELLIS_PAGE=""
-        if [ "$CURRENT_APP" = "Terminal" ]
-        then
-            TRELLIS_PAGE="page3";
-        elif [ "$CURRENT_APP" = "Qt Creator" ] || [ "$CURRENT_APP" = "studio" ] || [ "$CURRENT_APP" = "Xcode" ] 
+        TRELLIS_PAGE="page0"
+        if [ "$CURRENT_APP" = "Qt Creator" ] || [ "$CURRENT_APP" = "studio" ] || [ "$CURRENT_APP" = "Xcode" ] # develop
         then
             TRELLIS_PAGE="page1";
-        elif [ "$CURRENT_APP" = "firefox" ] || [ "$CURRENT_APP" = "Google Chrome" ]
+        elif [ "$CURRENT_APP" = "firefox" ] || [ "$CURRENT_APP" = "Google Chrome" ] # browsing
         then
             TRELLIS_PAGE="page2";
-        elif [ "$CURRENT_APP" = "Sublime Text 2" ]
+        elif [ "$CURRENT_APP" = "Sublime Text 2" ] # editing
         then
-            TRELLIS_PAGE="page0";
+            TRELLIS_PAGE="page3";
         fi
         mosquitto_pub -t toAtreyu -m "$TRELLIS_PAGE"
     fi 
 done &
 
-mosquitto_sub -t fromAtreyu | while read MESSAGE
-do
-if [ "$MESSAGE" = "0" ] #Page 0 #F11
+mosquitto_sub -t fromAtreyu | while read KEY
+do    
+if [ "$KEY" = "0" ] ## Exposé
 then
 osascript <<EOD
     tell application "System Events"
     key code 103
     end tell
 EOD
-elif [ "$MESSAGE" = "1" ] # # + Tab
+elif [ "$KEY" = "1" ] # # + Tab
 then
 osascript <<EOD
     tell application "System Events"
@@ -56,7 +53,7 @@ osascript <<EOD
         key up command
     end tell
 EOD
-elif [ "$MESSAGE" = "2" ]
+elif [ "$KEY" = "2" ]
 then
 sleep 2;
 osascript <<EOD
@@ -64,130 +61,58 @@ osascript <<EOD
         set frontApp to name of first application process whose frontmost is true
     end tell
 EOD
-elif [ "$MESSAGE" = "3" ]
+elif [ "$KEY" = "3" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "4" ]
+elif [ "$KEY" = "4" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "5" ]
+elif [ "$KEY" = "5" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "6" ]
+elif [ "$KEY" = "6" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "7" ]
+elif [ "$KEY" = "7" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "8" ]
+elif [ "$KEY" = "8" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "9" ]
+elif [ "$KEY" = "9" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "10" ]
+elif [ "$KEY" = "10" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "11" ]
+elif [ "$KEY" = "11" ]
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "12" ] #Page 1
+elif [ "$KEY" = "12" ] ##
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "13" ]
+elif [ "$KEY" = "13" ] # Search term global
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "14" ]
+elif [ "$KEY" = "14" ] # Go to parent
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "15" ]
+elif [ "$KEY" = "15" ] # Refactor
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "16" ]
+elif [ "$KEY" = "16" ] # Run App
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "17" ]
+elif [ "$KEY" = "17" ] # Debug App
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "18" ]
+elif [ "$KEY" = "18" ] # Clean App
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "19" ]
+elif [ "$KEY" = "19" ] # Show Console
 then
     echo "Neato cool"
-elif [ "$MESSAGE" = "20" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "21" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "22" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "23" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "24" ] #Page 2
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "25" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "26" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "27" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "28" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "29" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "30" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "31" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "32" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "33" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "34" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "35" ]
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "36" ] #Page 3 
-then
-    echo "Enga"
-elif [ "$MESSAGE" = "37" ] # Search term global
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "38" ] # Go to parent
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "39" ] # Refactor
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "40" ] # Run App
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "41" ] # Debug App
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "42" ] # Clean App
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "43" ] # Show Console
-then
-    echo "Neato cool"
-elif [ "$MESSAGE" = "44" ] # Debug Run
+elif [ "$KEY" = "20" ] # Debug Run
 then
 osascript <<EOD
     tell application "System Events"
@@ -204,35 +129,121 @@ osascript <<EOD
         end if
     end tell
 EOD
-elif [ "$MESSAGE" = "45" ] # Debug Next
+elif [ "$KEY" = "21" ] # Debug Next
 then
 osascript <<EOD
     tell application "System Events"
-        key down shift
-        key down command
-        key code 31
-        key up shift
-        key up command
+        set frontApp to name of first application process whose frontmost is true
+        if frontApp = "Qt Creator" then
+            key down shift
+            key down command
+            key code 31
+            key up shift
+            key up command
+        else if frontApp = "studio" then            
+        else if frontApp = "Xcode" then
+        end if
     end tell
 EOD
-elif [ "$MESSAGE" = "46" ] # Debug In
+elif [ "$KEY" = "22" ] # Debug In
 then
 osascript <<EOD
     tell application "System Events"
-        key down shift
-        key down command
-        key code 34
-        key up shift
-        key up command
+        set frontApp to name of first application process whose frontmost is true
+        if frontApp = "Qt Creator" then
+            key down shift
+            key down command
+            key code 34
+            key up shift
+            key up command
+        else if frontApp = "studio" then            
+        else if frontApp = "Xcode" then
+        end if
     end tell
 EOD
-else # Debug Exit
+elif [ "$KEY" = "23" ] # Debug Out
+then
 osascript <<EOD
     tell application "System Events"
-        key down command
-        key code 17
-        key up command
+        set frontApp to name of first application process whose frontmost is true
+        if frontApp = "Qt Creator" then
+            key down command
+            key code 17
+            key up command
+        else if frontApp = "studio" then            
+        else if frontApp = "Xcode" then
+        end if        
     end tell
 EOD
+elif [ "$KEY" = "24" ] ##
+then
+    echo "Neato cool"
+elif [ "$KEY" = "25" ] 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "26" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "27" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "28" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "29" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "30" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "31" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "32" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "33" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "34" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "35" ] # 
+then
+    echo "Neato cool"
+elif [ "$KEY" = "36" ] ##
+then
+    echo "Enga"
+elif [ "$KEY" = "37" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "38" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "39" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "40" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "41" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "42" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "43" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "44" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "45" ] #
+then
+    echo "Neato cool"
+elif [ "$KEY" = "46" ] #
+then
+elif [ "$KEY" = "47" ] #
+    echo "Neato cool"
 fi
 done
